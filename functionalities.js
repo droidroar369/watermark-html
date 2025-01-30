cad894a71260e04bfc003312bc6d067f0a5414af
Droid Roar <89954431+droidroar369@users.noreply.github.com>
2025-01-30 03:13:34 -0600

    Add files via upload

--- functionalities.js
+++ functionalities.js
@@ -16,12 +16,87 @@ var logo_chrgd= false;
 //Util variables
 var f_name;
 var W, H;
-var opacity= 25; //Percentage
-var scale=0.3; // 1=actual size of logo
+var obj_logo= {
+  opacity: 0.25, //0: transparent, 1: opaque
+  scale: 0.3, // 1: actual size of logo
+  posX: 0.5 - 0.5*0.3, //Relative upper-left X position of logo within the base image
+  posY: 0.5 - 0.5*0.3 //Relative upper-left Y position of logo within the base image
+};
+var prms= ["opacity", "scale", "posX", "posY"];
+var pressed_bars= [];
+var original_color_bars;
+
 
 //**************
 // LISTENERS
 //**************
+
+//Actions when document is loaded
+addEventListener("load", (e)=> {
+  //Set position of circle markers for each level bar
+  document.querySelectorAll(".bar-interval").forEach((i_bar,j) => {
+      //When the circle marks are loaded, position them at their initial values
+      var mark= i_bar.querySelector('.circle-marker');
+      var d_circ= mark.getBoundingClientRect().width;
+      
+      mark.style.left= ( obj_logo[prms[j]] * (i_bar.offsetWidth - d_circ) )+ "px";
+      pressed_bars[j]= false;
+      
+      if(j==0) original_color_bars= i_bar.style.background; //Save the original background
+      
+      i_bar.style.background= "rgb(118, 118, 204)"; //Background when bars disabled
+  });
+
+  Telegram.WebApp.ready();
+
+});
+
+
+//When click mouse ends, the pressed bar is unpressed
+addEventListener("mouseup", unpress_bar);
+
+
+
+//Movility for touch devices. If touch end, call the mouseup listener
+addEventListener("touchend", unpress_bar);
+
+
+
+
+//When mouse is moving if some bar is pressed, move its mark
+addEventListener('mousemove', (e) => {
+  //Detect the pressed bar
+  var j= pressed_bars.findIndex((elem) => elem);
+  if(j==-1) return; //Exit if no bar is pressed
+  
+  var marker = document.querySelectorAll('.bar-interval')[j].querySelector('.circle-marker');
+  
+  //Just move when imgs have been loaded
+  if(!img_chrgd || !logo_chrgd || marker===null) { return; }
+  
+  move_marker(marker, e.clientX, j); //Move the mark and set the level of its variable
+  
+});
+
+
+
+//Movility for touch devices. If there's movement while screen is pulsed, call the mousemove listener
+addEventListener('touchmove', (e) => {
+  var j= pressed_bars.findIndex((elem) => elem);
+  if(j==-1) return;
+
+  //Dispatch a mouse event
+  //e.preventDefault();
+  dispatchEvent(new MouseEvent("mousemove", {
+    bubbles: true,
+    cancelable: true,
+    clientX: e.touches[0].clientX,
+    clientY: e.touches[0].clientY
+  }));
+  
+});
+
+
 
 
 //Add listener to the image file button
@@ -50,23 +125,15 @@ img_btn.addEventListener("change", (ev)=> {
           cnv.width = img.width;
           cnv.height = img.height;
           W= cnv.width; H= cnv.height;
+
           // Draw the image in the canvas
           ctx.drawImage(img, 0, 0);
-
-          // Draw the horizontal green line
-          //ctx.strokeStyle = 'green';
-          //ctx.lineWidth = 5;
-          //ctx.beginPath();
-          //ctx.moveTo(0, img.height / 2);
-          //ctx.lineTo(img.width, img.height / 2);
-          //ctx.stroke();
       };
       img.src = e.target.result;
   };
   reader.readAsDataURL(file);
 
   enable_button(logo_btn.parentNode);
-  //enable_button(dwnl_btn.parentNode);
   img_chrgd= true;
 });
 
@@ -84,6 +151,12 @@ logo_btn.addEventListener("change", (ev)=> {
     return;
   }
   
+  //Force the img_btn to redraw the base image, to erase the old logo
+  if(logo_chrgd) img_btn.dispatchEvent(new Event("change"));
+  else {
+    document.querySelectorAll('.bar-interval').forEach((i_bar) => {i_bar.style.background= original_color_bars;} )
+  }
+  
 
   //When the logo file is loaded, we follow with the algorithm
   const reader = new FileReader();
@@ -91,26 +164,7 @@ logo_btn.addEventListener("change", (ev)=> {
   reader.onload = (e) => {
       const img = new Image();
       
-      img.onload = () => {
-          //Width and height of LOGO image
-          var w=img.width; var h=img.height;
-          
-          //Set the hidden canvas logo
-          cnv_logo.width= w; cnv_logo.height= h;
-          ctx_logo.drawImage(img,0,0);
-
-          // Make transparent the canvas manipulating each pixel with the "data" property
-          var logo_data= ctx_logo.getImageData(0,0,w,h);
-          var data= logo_data.data;
-          for(var i=3; i<data.length; i+=4) {
-            data[i]= Math.round(255* opacity/100);
-          }
-          logo_data.data = data;
-          ctx_logo.putImageData(logo_data,0,0);
-
-          //Draw the logo on the original context
-          ctx.drawImage(cnv_logo, (0.5-0.15)*W, (0.5-0.15)*H, scale*W, scale*W *h/w );
-      };
+      img.onload = () => draw_logo(img);
       img.src = e.target.result;
   };
   reader.readAsDataURL(file);
@@ -122,8 +176,8 @@ logo_btn.addEventListener("change", (ev)=> {
 
 
 //Prevent logo click if image is not charged
-logo_btn.addEventListener('click', (ev) => {
-  if(!img_chrgd) { ev.preventDefault(); return; }
+logo_btn.addEventListener('click', (eve) => {
+  if(!img_chrgd) { e.preventDefault(); return; }
 });
 
 
@@ -147,7 +201,49 @@ dwnl_btn.addEventListener('click', (ev) => {
             link.click();
             
             //Download (Telegram method)
-            window.Telegram.WebApp.showAlert("Native Telegram Downlod is still no available");
+            Telegram.WebApp.downloadFile({
+              url: link.href,
+              file_name: link.download
+            });
+});
+
+
+
+//Listeners for all the interval bars
+document.querySelectorAll('.bar-interval').forEach((i_bar, j) => {
+  
+  //With click at a point of the bar, set the opacity level and move the mark circle
+  i_bar.addEventListener('click', (e) => {
+      //If some of the images has not been loaded, exit from the function
+      if(!img_chrgd || !logo_chrgd) { return; }
+
+      var marker = e.target.querySelector('.circle-marker');
+      //If marker is null (it happens when the user clicks over the mark), stop the function
+      if(marker === null) { return; }
+      
+      //Move marker and set its corresponding variable
+      move_marker(marker, e.clientX, j);
+      
+      //Dispatch the logo file charger, to redraw the canvas
+      logo_btn.dispatchEvent(new Event("change"));
+      
+  });
+  
+  
+  
+  //If some bar is pressed (when is enabled because img and logo are selected), change its flag to true
+  i_bar.addEventListener('mousedown', (e) => {
+    if(img_chrgd && logo_chrgd) pressed_bars[j] = true;
+  });
+  
+  
+  
+  //Movility for touch devices, it screams when touch starts
+  i_bar.addEventListener('touchstart', (e) => {
+    if(img_chrgd && logo_chrgd) pressed_bars[j] = true;
+  });
+
+
 });
 
 
@@ -159,7 +255,7 @@ dwnl_btn.addEventListener('click', (ev) => {
 
 /**
 * Disables a button, changing its aspect and disabling the click
-* @param btn_mask: The div mask container for the true button
+* @param btn_mask The div mask container for the true button
 */
 function disable_button(btn_mask) {
   //Change the aspect class, and disables the button
@@ -172,7 +268,7 @@ function disable_button(btn_mask) {
 
 /**
 * Enables a button, changing its aspect and enabling the click
-* @param btn_mask: The div mask container for the true button
+* @param btn_mask The div mask container for the true button
 */
 function enable_button(btn_mask) {
   //Change the aspect class, and enables the button
@@ -184,13 +280,71 @@ function enable_button(btn_mask) {
 
 
 
+/**
+* Draws the logo with a degree of opacity over the base image
+* @param l_img The logo image
+*/
+function draw_logo(l_img) {
+    //Width and height of LOGO image
+    var w= l_img.width; var h= l_img.height;
+    
+    //Set the hidden canvas logo
+    cnv_logo.width= w; cnv_logo.height= h;
+    ctx_logo.drawImage(l_img,0,0);
+    
+    // Make transparent the canvas manipulating each pixel with the "data" property
+    var logo_data= ctx_logo.getImageData(0,0,w,h);
+    var data= logo_data.data;
+    for(var i=3; i<data.length; i+=4) {
+      data[i]= Math.round(255* obj_logo["opacity"]);
+    }
+    logo_data.data = data;
+    ctx_logo.putImageData(logo_data,0,0);
+    
+    //Draw the logo on the original context
+    ctx.drawImage(cnv_logo, obj_logo["posX"]*W, obj_logo["posY"]*H, obj_logo["scale"]*W, obj_logo["scale"]*W *h/w );
+}
 
 
 
 
+/**
+ * Moves the circle marker within an interval bar, and sets the reletive level (lvl in [0:1]) according to its position
+ * @param marker A DOM object within the interval bar, representing the circle mark
+ * @param x The "x" coordinate where the mark must be moved
+ * @param j The bar index whose level will be changed
+ */
+function move_marker(marker, x, j) {
+    var rect = marker.getBoundingClientRect();
+    var d_circ= rect.width; //Diameter of circle
+    var i_bar= marker.parentNode; //Interval bar
+    var L= i_bar.offsetWidth - d_circ; //Length of bar
+    
+    //Left X coordinate for the circle
+    var markX = x - i_bar.style.left - 0.8*d_circ;
+    markX= (markX<0)? 0: markX;
+    markX= (markX>L)? L: markX;
+    
+    //Move the circle marker
+    marker.style.left = markX + "px";
+
+    //Set the corresponding obj_logo property with the percentage
+    if(j!=-1) obj_logo[prms[j]]= markX/L;
+}
 
 
 
+/**
+ * Set to false the first true entry from "pressed_bars" array (if there's one), and dispatch the change in "logo_btn" to upload the canvas
+ */
+
+function unpress_bar() {
+  var j= pressed_bars.findIndex((elem) => elem);
+  if(j!=-1) {
+    logo_btn.dispatchEvent(new Event("change"));
+    pressed_bars[j] = false;
+  }
+}
 
 
 
